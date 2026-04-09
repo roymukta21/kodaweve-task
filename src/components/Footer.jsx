7cc86132949076c777c7fabfd56e40b990618e83
MUKTA ROY <muktaroy520@gmail.com>
2026-04-09 22:27:43 +0600

    feat(Footer): update grid layout to improve responsiveness and structure

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router";
 export default function Footer() {
   return (
     <footer className="bg-gray-900 text-gray-300 px-6 py-10">
-      <div className="max-w-7xl mx-auto grid md:grid-cols-3 gap-8">
+      <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-4  gap-8">
         {/* Brand */}
          <div className="col-span-1 md:col-span-2 space-y-6">
           <Link to="/" className="flex items-center gap-2">
